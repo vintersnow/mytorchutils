d9b671625e79f4e395201cb6334779b027dfcd27
vinter <ankasandora@gmail.com>
2018-06-08 17:52:50 +0900

    change: reader

--- data/reader.py
+++ data/reader.py
@@ -1,5 +1,8 @@
 from os import path
 import mmap
+import pickle
+import numpy as np
+import json
 
 
 def utf8len(s):
@@ -7,10 +10,12 @@ def utf8len(s):
 
 
 class Reader(object):
-    '''Allow Random read using index file'''
-    def __init__(self, data_file, idx_file=None):
+    def __init__(self, data_file, idx_file=None, protocol='pickle'):
         self.data_file = data_file
         self.idx_file = idx_file if idx_file else data_file + '.idx'
+
+        assert protocol == 'pickle' or protocol == 'json'
+        self.protocol = protocol
 
         if not path.exists(self.idx_file):
             self.create_idx()
@@ -24,15 +29,21 @@ class Reader(object):
 
     def create_idx(self):
         lines = open(self.data_file, 'r').read().split('\n')
-        idx = [utf8len(d) + 1 for d in lines]
-        with open(self.idx_file, 'w') as f:
-            s = 0
-            for i in idx:
-                f.write('%d,' % s)
-                s += i
+        idx = np.asarray([0] + [utf8len(d) + 1 for i, d in enumerate(lines)])
+        idx = np.cumsum(idx)
+
+        if self.protocol == 'pickle':
+            with open(self.idx_file, 'wb') as f:
+                pickle.dump(idx, f, protocol=pickle.HIGHEST_PROTOCOL)  # 800ms
+        else:
+            with open(self.idx_file, 'w') as f:
+                json.dump(idx.tolist(), f)  # 1633ms
 
     def load_idx(self):
-        return [int(i) for i in open(self.idx_file, 'r').read().split(',') if i]
+        if self.protocol == 'pickle':
+            return pickle.load(open(self.idx_file, 'rb'))  # 44ms
+        else:
+            return json.load(open(self.idx_file, 'r'))  # 110ms
 
     def __getitem__(self, i):
         idx = self.idx[i]
@@ -41,18 +52,3 @@ class Reader(object):
 
     def __len__(self):
         return len(self.idx) - 1
-
-
-def test(file):
-    r = Reader(file)
-
-    print(r[0])
-    print(r[1])
-    print(r[2])
-    print(r[3])
-    print(len(r))
-    print(r.idx)
-
-
-if __name__ == '__main__':
-    test('test/data/a')
