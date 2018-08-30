5699eff36711e72711fb530e60ed88a4f0705912
vinter <ankasandora@gmail.com>
2018-08-30 14:52:46 +0900

    add: line dataset

--- data/dataset.py
+++ data/dataset.py
@@ -2,33 +2,61 @@ from torch.utils.data import Dataset
 from os import path
 from .reader import Reader
 import json
+from abc import abstractmethod
 
 
-class JsonLineDataset(Dataset):
-    def __init__(self, file, transform=None):
+class BaseDataset(Dataset):
+    def __init__(self, file, transformer=None, save_trans=True):
         assert path.isfile(file), file
         self.file = file
-        self.data = Reader(file)
+        self.data = None
 
-        self.transform = transform
+        self.transformer = transformer
         self.transformed = [None] * len(self.data)
+        self.save_trans = save_trans
 
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, idx):
-        if self.transformed[idx]:
-            return self.transformed[idx]
-
-        sample = json.loads(self.data[idx])
-
-        if self.transform:
-            if isinstance(self.transform, list):
-                for c in self.transform:
+    def transorm(self, sample):
+        if self.transformer:
+            if isinstance(self.transformer, list):
+                for c in self.transformer:
                     sample = c(sample)
             else:
-                sample = self.transform(sample)
+                sample = self.transformer(sample)
+        return sample
 
-        self.transformed[idx] = sample
+    @abstractmethod
+    def prepare(self, idx):
+        pass
+
+    def __getitem__(self, idx):
+        if self.save_trans and self.transformed[idx]:
+            return self.transformed[idx]
+
+        sample = self.prepare(idx)
+
+        sample = self.transorm(sample)
+        if self.save_trans:
+            self.transformed[idx] = sample
 
         return sample
+
+
+class LineDataset(BaseDataset):
+    def __init__(self, *args, **keys):
+        super(LineDataset, *args, **keys)
+        self.data = Reader(self.file)
+
+    def prepare(self, idx):
+        return self.data[idx]
+
+
+class JsonLineDataset(BaseDataset):
+    def __init__(self, *args, **keys):
+        super(JsonLineDataset, self).__init__(*args, **keys)
+        self.data = Reader(self.file)
+
+    def prepare(self, idx):
+        return json.loads(self.data[idx])
