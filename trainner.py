5c7eb80b54cd81ea6dc81e929c4b029f7fde7261
vinter <ankasandora@gmail.com>
2018-12-21 01:39:08 +0900

    change: batcher

--- trainner.py
+++ trainner.py
@@ -28,29 +28,42 @@ class Context(object):
 class Runner(object):
     def __init__(self, model: Model, summary_prefix: str) -> None:
         self.ctx = Context()
-        self.olp = OneLinePrint(' | ')
+        self.olp = OneLinePrint(" | ")
         self.model = model
         self.summary_prefix = summary_prefix
 
     def write_summary(self, step: int) -> None:
         for key, val in self.ctx.scalar.items():
-            key = f'{self.summary_prefix}/{key}'
+            key = f"{self.summary_prefix}/{key}"
             self.model.writer.add_scalar(key, val, step)
 
     def log_str(self) -> str:
         items = sorted(self.ctx.log.items(), key=lambda x: x[0])
-        return ', '.join([f'{key}: {float(val):.3f}' for key, val in items])
+        return ", ".join([f"{key}: {float(val):.3f}" for key, val in items])
 
-    def run(self,
-            train_ld: DataLoader,
-            val_ld: DataLoader,
-            init_epoch: int = 0,
-            max_epoch: int = 5,
-            max_step_each_epoch: int = 0,
-            save_model: bool = False,
-            summary_step: int = 0,
-            clear_log_each_step: bool = True):
-        epoch_length = min(max_step_each_epoch, len(train_ld)) if max_step_each_epoch > 0 else len(train_ld)
+    def batch_gen(self, ld):
+        while True:
+            for batch in ld:
+                yield batch
+
+    def run(
+        self,
+        train_ld: DataLoader,
+        val_ld: DataLoader,
+        init_epoch: int = 0,
+        max_epoch: int = 5,
+        max_step_each_epoch: int = 0,
+        save_model: bool = False,
+        summary_step: int = 0,
+        clear_log_each_step: bool = True,
+    ):
+        epoch_length = (
+            min(max_step_each_epoch, len(train_ld))
+            if max_step_each_epoch > 0
+            else len(train_ld)
+        )
+
+        self.train_batcher = self.batch_gen(train_ld)
 
         for epoch in range(init_epoch, max_epoch):
             self.ctx.epoch = epoch
@@ -58,9 +71,8 @@ class Runner(object):
             self.model.train()
             self.ctx._clear()
             elapsed: float = 0
-            for i, batch in enumerate(train_ld):
-                if i >= epoch_length:
-                    break
+            for i in range(epoch_length):
+                batch = next(self.train_batcher)
 
                 if clear_log_each_step:
                     self.ctx._clear()
@@ -72,18 +84,26 @@ class Runner(object):
                 elapsed += time.time() - start
 
                 if math.isnan(loss):
-                    raise ValueError(f'loss is nan, epoch {epoch}, step {step}')
+                    raise ValueError(f"loss is nan, epoch {epoch}, step {step}")
 
                 with self.olp as w:
                     # logging
-                    t = elapsed / (min(i, (step % summary_step) if summary_step > 0 else float('inf')) + 1)
-                    w(f'{self.model.name} epoch {epoch + 1}/{max_epoch} step {(step % epoch_length) + 1}/{epoch_length} '
-                      f'sec/step={t:.3f}')
+                    t = elapsed / (
+                        min(
+                            i,
+                            (step % summary_step) if summary_step > 0 else float("inf"),
+                        )
+                        + 1
+                    )
+                    w(
+                        f"{self.model.name} epoch {epoch + 1}/{max_epoch} step {(step % epoch_length) + 1}/{epoch_length} "
+                        f"sec/step={t:.3f}"
+                    )
                     w(self.log_str())
 
                 # write summary
                 if summary_step > 0 and (step + 1) % summary_step == 0:
-                    self.ctx.scalar['time'] = elapsed / min(i + 1, summary_step)
+                    self.ctx.scalar["time"] = elapsed / min(i + 1, summary_step)
                     elapsed = 0
                     self.write_summary(step)
 
