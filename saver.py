2be9dfa71af0fbe9f3758ccb0eb54b8d7f600541
vinter <ankasandora@gmail.com>
2018-12-21 00:48:31 +0900

    add: step load

--- saver.py
+++ saver.py
@@ -14,28 +14,30 @@ ckpt_t = Tuple[Optional[str], Optional[int], Optional[float]]
 
 
 class MutliParamSaver(object):
-    def __init__(self,
-                 log_dir: str,
-                 cont: bool = False,
-                 metric: Optional[str] = 'lower',
-                 keep_all: bool = True) -> None:
-        assert metric == 'lower' or metric == 'higher' or metric is None, metric
+    def __init__(
+        self,
+        log_dir: str,
+        cont: bool = False,
+        metric: Optional[str] = "lower",
+        keep_all: bool = True,
+    ) -> None:
+        assert metric == "lower" or metric == "higher" or metric is None, metric
         if metric is None:
             assert keep_all
         self._log_dir = log_dir
         self.params: Dict = {}
         self.metric = metric
         self.keep_all = keep_all
-        self.format = 'step-%d_score-%.3f'
+        self.format = "step-%d_score-%.3f"
 
         if cont:
             if path.isdir(self._log_dir):
                 return
             else:
-                raise ValueError('No dir to continue: %s' % self._log_dir)
+                raise ValueError("No dir to continue: %s" % self._log_dir)
         else:
             if path.isdir(self._log_dir):
-                raise ValueError('log directory already exist: %s' % self._log_dir)
+                raise ValueError("log directory already exist: %s" % self._log_dir)
 
         makedirs(self._log_dir)
 
@@ -43,11 +45,15 @@ class MutliParamSaver(object):
         rmtree(dir, ignore_errors=True)
 
     def ckpt_list(self) -> List[ckpt_t]:
-        ckpts = glob.glob(path.join(self._log_dir, '*'))
+        ckpts = glob.glob(path.join(self._log_dir, "*"))
         # TODO: support format
-        reg = re.compile(r'.*step-([0-9]+)_score-([0-9]+.[0-9]+).*')
+        reg = re.compile(r".*step-([0-9]+)_score-([0-9]+.[0-9]+).*")
         dirs = [reg.search(f) for f in ckpts]
-        dir_tuple = [cast(ckpt_t, (str(r.group(0)), int(r.group(1)), float(r.group(2)))) for r in dirs if r is not None]
+        dir_tuple = [
+            cast(ckpt_t, (str(r.group(0)), int(r.group(1)), float(r.group(2))))
+            for r in dirs
+            if r is not None
+        ]
         return dir_tuple
 
     def best(self, metric: Optional[str]) -> ckpt_t:
@@ -55,9 +61,9 @@ class MutliParamSaver(object):
         if len(files) == 0:
             # logger.error('No ckpt found in %s' % self._log_dir)
             return None, None, None
-        elif metric == 'higher':
+        elif metric == "higher":
             mm = max
-        elif metric == 'lower':
+        elif metric == "lower":
             mm = min
         else:
             # logger.error('Unknown metric: %s' % metric)
@@ -71,6 +77,13 @@ class MutliParamSaver(object):
         else:
             return None, None, None
 
+    def load_by_step(self, step):
+        files = self.ckpt_list()
+        for ckpt in files:
+            if ckpt[1] == step:
+                return ckpt
+        return None, None, None
+
     def save(self, step: int, score: Union[int, float]) -> None:
         best_dir, best_step, best_score = self.best(self.metric)
         latest_dir, latest_step, _ = self.latest()
@@ -80,26 +93,29 @@ class MutliParamSaver(object):
         dir = path.join(self._log_dir, self.format % (step, score))
 
         if path.isdir(dir):
-            logger.error('log dir for step %d exisit' % step)
+            logger.error("log dir for step %d exisit" % step)
             return
 
         makedirs(dir)
 
         for key, val in self.params.items():
-            file_path = path.join(dir, key + '.ckpt')
+            file_path = path.join(dir, key + ".ckpt")
             torch.save(val.state_dict(), file_path)
 
         if not self.keep_all and best_score is not None:
-            sign = 1 if self.metric == 'higher' else -1
+            sign = 1 if self.metric == "higher" else -1
             if (score - best_score) * sign > 0 and best_dir is not None:
                 self.rm_ckpt(best_dir)
             if best_step != latest_step and latest_dir is not None:
                 self.rm_ckpt(latest_dir)
 
     def load_ckpt(self, method) -> Tuple[Dict[str, Any], int, str]:
-        if method == 'highest' or method == 'lowest':
+        if method == "highest" or method == "lowest":
             # assert self.metric == method
-            dir, step, score = self.best(method.replace('est', 'er'))
+            dir, step, score = self.best(method.replace("est", "er"))
+        elif method.isdigit():
+            dir, step, score = self.load_by_step(int(method))
+            assert step == int(method)
         else:
             dir, step, score = self.latest()
 
@@ -107,36 +123,32 @@ class MutliParamSaver(object):
         assert score is not None
         assert isinstance(dir, str)
 
-        ckpts = glob.glob(path.join(dir, '*'))
+        ckpts = glob.glob(path.join(dir, "*"))
 
         params = {}
         for ckpt in ckpts:
             p = torch.load(ckpt)
-            key = path.basename(ckpt).replace('.ckpt', '')
+            key = path.basename(ckpt).replace(".ckpt", "")
             params[key] = p
 
         return params, step, dir
 
-    def add_param(self, key: str,
-                  val: Union[torch.nn.Module, torch.optim.Optimizer]) -> None:
+    def add_param(
+        self, key: str, val: Union[torch.nn.Module, torch.optim.Optimizer]
+    ) -> None:
         self.params[key] = val
 
 
 class Saver(object):
     # TODO: change ckpt file name 'loss' -> 'score'
-    def __init__(self,
-                 model,
-                 opt=None,
-                 cont=False,
-                 metric='lower',
-                 keep_all=True):
-        '''
+    def __init__(self, model, opt=None, cont=False, metric="lower", keep_all=True):
+        """
         Args:
             cont (bool): Trueなら新しいディレクトリを作らない
-        '''
-        logger.warning('Duplicated: Use MutliParamSaver')
-        assert metric == 'lower' or metric == 'higher', metric
-        self._log_dir = path.join(model.log_dir, 'train')
+        """
+        logger.warning("Duplicated: Use MutliParamSaver")
+        assert metric == "lower" or metric == "higher", metric
+        self._log_dir = path.join(model.log_dir, "train")
         self._name = model.name
         self._model = model.model
         self._opt = opt
@@ -147,31 +159,34 @@ class Saver(object):
             if path.isdir(self._log_dir):
                 return
             else:
-                raise ValueError('No dir to continue: %s' % self._log_dir)
+                raise ValueError("No dir to continue: %s" % self._log_dir)
 
         # move dir if already exists
         if path.isdir(self._log_dir):
             for i in itertools.count():
-                bk_path = self._log_dir + '.' + str(i)
+                bk_path = self._log_dir + "." + str(i)
                 if not path.isdir(bk_path):
                     rename(self._log_dir, bk_path)
                     break
         makedirs(self._log_dir)
 
-        with open(path.join(self._log_dir, self._name + '.model'), 'w') as f:
+        with open(path.join(self._log_dir, self._name + ".model"), "w") as f:
             f.write(str(model))
 
     def ckpt_list(self):
-        ckpts = glob.glob(path.join(self._log_dir, '*[0-9].ckpt'))
-        reg = re.compile(r'.*step-([0-9]+)_loss-([0-9]+.[0-9]+).*')
+        ckpts = glob.glob(path.join(self._log_dir, "*[0-9].ckpt"))
+        reg = re.compile(r".*step-([0-9]+)_loss-([0-9]+.[0-9]+).*")
         files = [reg.search(f) for f in ckpts]
-        files = [(r.group(0), int(r.group(1)), float(r.group(2)))
-                 for r in files if r is not None]
+        files = [
+            (r.group(0), int(r.group(1)), float(r.group(2)))
+            for r in files
+            if r is not None
+        ]
         return files
 
     def best(self):
         files = self.ckpt_list()
-        mm = max if self.metric == 'higher' else min
+        mm = max if self.metric == "higher" else min
         if len(files) > 0:
             return mm(files, key=lambda x: x[2])
         else:
@@ -185,7 +200,7 @@ class Saver(object):
             return None, None, None
 
     def rm_ckpt(self, step):
-        ckpts = glob.glob(path.join(self._log_dir, 'step-%d_*.ckpt' % step))
+        ckpts = glob.glob(path.join(self._log_dir, "step-%d_*.ckpt" % step))
         for file in ckpts:
             if path.isfile(file):
                 remove(file)
@@ -194,33 +209,33 @@ class Saver(object):
         best_file, best_step, best_score = self.best()
         latest_file, latest_step, _ = self.latest()
 
-        file_name = 'step-%d_loss-%.3f.ckpt' % (step, score)
+        file_name = "step-%d_loss-%.3f.ckpt" % (step, score)
         file_path = path.join(self._log_dir, file_name)
         torch.save(self._model.state_dict(), file_path)
         if self._opt:
-            opt_file_name = 'step-%d_loss-%.3f_opt.ckpt' % (step, score)
+            opt_file_name = "step-%d_loss-%.3f_opt.ckpt" % (step, score)
             opt_file_path = path.join(self._log_dir, opt_file_name)
             torch.save(self._opt.state_dict(), opt_file_path)
 
         if not self.keep_all and best_score is not None:
-            sign = 1 if self.metric == 'higher' else -1
+            sign = 1 if self.metric == "higher" else -1
             if (score - best_score) * sign > 0:
                 self.rm_ckpt(best_step)
             if best_step != latest_step:
                 self.rm_ckpt(latest_step)
 
 
-def load_ckpt(model, method='latest'):
-    logger.warning('Duplicated: Use MutliParamSaver')
-    ckpt_dir = path.join(model.log_dir, 'train')
-    model_list = glob.glob(path.join(ckpt_dir, '*[0-9].ckpt'))
-    opt_list = glob.glob(path.join(ckpt_dir, '*_opt.ckpt'))
+def load_ckpt(model, method="latest"):
+    logger.warning("Duplicated: Use MutliParamSaver")
+    ckpt_dir = path.join(model.log_dir, "train")
+    model_list = glob.glob(path.join(ckpt_dir, "*[0-9].ckpt"))
+    opt_list = glob.glob(path.join(ckpt_dir, "*_opt.ckpt"))
 
     if len(model_list) == 0:
-        raise ValueError('No ckpt file is found in %s' % ckpt_dir)
+        raise ValueError("No ckpt file is found in %s" % ckpt_dir)
 
     def latest(files):
-        reg = re.compile(r'.*step-([0-9]+)_.*')
+        reg = re.compile(r".*step-([0-9]+)_.*")
         files = [reg.search(f) for f in files]
         files = [(r.group(0), int(r.group(1))) for r in files if r is not None]
         if len(files) > 0:
@@ -230,10 +245,13 @@ def load_ckpt(model, method='latest'):
 
     def best(files, m):
         # TODO: formatを変えれるもしくは、lossではない単語にした方が良い
-        reg = re.compile(r'.*step-([0-9]+)_loss-([0-9]+.[0-9]+).*')
+        reg = re.compile(r".*step-([0-9]+)_loss-([0-9]+.[0-9]+).*")
         files = [reg.search(f) for f in files]
-        files = [(r.group(0), int(r.group(1)), float(r.group(2)))
-                 for r in files if r is not None]
+        files = [
+            (r.group(0), int(r.group(1)), float(r.group(2)))
+            for r in files
+            if r is not None
+        ]
 
         if len(files) > 0:
             return m(files, key=lambda x: x[2])
@@ -242,28 +260,28 @@ def load_ckpt(model, method='latest'):
 
     def from_file(file):
         assert file in model_list
-        opt_file = file[:len(file) - len('.ckpt')] + '_opt.ckpt'
+        opt_file = file[: len(file) - len(".ckpt")] + "_opt.ckpt"
         if not path.isfile(opt_file):
             opt_file = None
-        reg = re.compile(r'.*step-([0-9]+)_loss-([0-9]+.[0-9]+).*')
+        reg = re.compile(r".*step-([0-9]+)_loss-([0-9]+.[0-9]+).*")
         r = reg.search(file)
         step = int(r.group(1))
         return file, opt_file, step
 
-    if method == 'latest':
+    if method == "latest":
         ckpt_model, model_step = latest(model_list)
         ckpt_opt, opt_step = latest(opt_list)
-    elif method == 'highest':
+    elif method == "highest":
         ckpt_model, model_step, model_loss = best(model_list, max)
         ckpt_opt, opt_step, _ = best(opt_list, max)
-    elif method == 'lowest':
+    elif method == "lowest":
         ckpt_model, model_step, model_loss = best(model_list, min)
         ckpt_opt, opt_step, _ = best(opt_list, min)
     elif path.isfile(method):
         ckpt_model, ckpt_opt, model_step = from_file(method)
         opt_step = model_step
     else:
-        raise ValueError('Unknown method or file not exists: %s' % method)
+        raise ValueError("Unknown method or file not exists: %s" % method)
 
     model.model.load_state_dict(torch.load(ckpt_model))
     if model.opt is None:
@@ -276,13 +294,13 @@ def load_ckpt(model, method='latest'):
     return model_step, ckpt_model
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # from .model import Model
     ly = torch.nn.Linear(10, 10)
     opt = torch.optim.SGD(ly.parameters(), 1)
-    saver = MutliParamSaver('test')
-    saver.params['model'] = ly
-    saver.params['opt'] = opt
+    saver = MutliParamSaver("test")
+    saver.params["model"] = ly
+    saver.params["opt"] = opt
     # m = Model(ly, 'test', '.')
     # m.addopt(torch.optim.SGD(ly.parameters(), 1))
     for i in range(10):
@@ -290,5 +308,5 @@ if __name__ == '__main__':
         opt.step()
         # m.save(i, i)
 
-    ppp = saver.load_ckpt('latest')
+    ppp = saver.load_ckpt("latest")
     print(ppp)
