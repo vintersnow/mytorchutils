f47cbfda4032b4321e0eb53fc9c3ef3c1323d69c
vinter <ankasandora@gmail.com>
2018-12-21 16:59:23 +0900

    add: override getattr for model.model

--- model.py
+++ model.py
@@ -9,14 +9,16 @@ from typing import Optional, Dict, Tuple
 
 
 class Model(nn.Module):
-    def __init__(self,
-                 model: nn.Module,
-                 name: str,
-                 log_root: Optional[str] = None,
-                 opts: dict = {},
-                 hps: Optional[Dict] = None,
-                 metric: Optional[str] = None,
-                 cont: bool = False) -> None:
+    def __init__(
+        self,
+        model: nn.Module,
+        name: str,
+        log_root: Optional[str] = None,
+        opts: dict = {},
+        hps: Optional[Dict] = None,
+        metric: Optional[str] = None,
+        cont: bool = False,
+    ) -> None:
         super(Model, self).__init__()
 
         self.model = model
@@ -33,39 +35,42 @@ class Model(nn.Module):
 
             if not path.isdir(self.log_dir):
                 makedirs(self.log_dir)
-            config_file = path.join(self.log_dir, 'config.json')
+            config_file = path.join(self.log_dir, "config.json")
             if hps and not path.isfile(config_file):
                 json.dump(
                     vars(hps),
-                    open(config_file, 'w'),
+                    open(config_file, "w"),
                     default=lambda x: x.name,
                     ensure_ascii=False,
                     indent=4,
                     sort_keys=True,
-                    separators=(',', ': '))
+                    separators=(",", ": "),
+                )
 
             # print out model info
-            with open(path.join(self.log_dir, 'model_arch'), 'w') as f:
+            with open(path.join(self.log_dir, "model_arch"), "w") as f:
                 f.write(str(model))
 
             # Saver
-            ckpt_dir = path.join(self.log_dir, 'train')
+            ckpt_dir = path.join(self.log_dir, "train")
             keep_all = metric is None
-            self.saver = MutliParamSaver(ckpt_dir, cont=cont, metric=metric, keep_all=keep_all)
-            self.saver.add_param('model', model)
+            self.saver = MutliParamSaver(
+                ckpt_dir, cont=cont, metric=metric, keep_all=keep_all
+            )
+            self.saver.add_param("model", model)
 
         self.training = True
 
     def forward(self, *args, **keys):
         return self.model(*args, **keys)
 
-    def restore(self, method: str = 'latest') -> Tuple[int, str]:
+    def restore(self, method: str = "latest") -> Tuple[int, str]:
         if self.nolog:
-            raise ValueError('No log directory')
+            raise ValueError("No log directory")
         params, step, dir = self.saver.load_ckpt(method)
         assert path.exists(dir)
-        self.model.load_state_dict(params['model'])
-        params.pop('model', None)
+        self.model.load_state_dict(params["model"])
+        params.pop("model", None)
 
         for key, val in params.items():
             if key in self.opts:
@@ -74,20 +79,16 @@ class Model(nn.Module):
 
     def save(self, step: int, loss: float) -> None:
         if self.nolog:
-            raise ValueError('No log directory')
+            raise ValueError("No log directory")
 
         self.saver.save(step, loss)
 
-    def make_writer(self, comment: str = '') -> SummaryWriter:
+    def make_writer(self, comment: str = "") -> SummaryWriter:
         if self.nolog:
-            raise ValueError('No log directory')
-        summary_dir = path.join(self.log_dir, 'summary')
+            raise ValueError("No log directory")
+        summary_dir = path.join(self.log_dir, "summary")
         self._writer = SummaryWriter(summary_dir, comment)
         return self._writer
-
-    def __del__(self) -> None:
-        if hasattr(self, '_writer'):
-            self._writer.close()
 
     def add_opt(self, key: str, opt: torch.optim.Optimizer) -> None:
         self.opts[key] = opt
@@ -101,6 +102,13 @@ class Model(nn.Module):
 
     @property
     def writer(self) -> SummaryWriter:
-        if not hasattr(self, '_writer'):
+        if not hasattr(self, "_writer"):
             return self.make_writer(self)
         return self._writer
+
+    def __del__(self) -> None:
+        if hasattr(self, "_writer"):
+            self._writer.close()
+
+    def __getattr__(self, attr):
+        return getattr(self.model, attr)
